531accdebd5ecd0eeea7c250fe155c0510ec9ee7
goranaviani <goranaviani@gmail.com>
2022-08-28 11:51:47 +0200

    filter data

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ const evensOnly = (data) => {
     return data.filter( x => x % 2 === 0)
 }
 
+const fiveCharactersOrFewerOnly = (data) => {
+    return data.filter( x => x.length <= 5)
+}
+
+
 function App() {
     const fiveAndGreater = fiveAndGreaterOnly([3, 6, 8, 2]); /// [6, 8]
     const evens = evensOnly([3, 6, 8, 2]); /// [6, 8, 2]
+    const chars = fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]); // ["by", "dog", "wolf", "eaten"]
   return (
     <>
       fiveAndGreater: {fiveAndGreater.map(x => <p> {x} </p>)}
         evensOnly = {evens.map ( x=> <>{x}, </> )}
+        fiveCharactersOrFewerOnly = {chars.map ( x=> <li>{x}</li>)}
       <br/>
     </>
   );
