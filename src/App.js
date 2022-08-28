50143449eafa79d4042127c3bdc304cfd921e138
goranaviani <goranaviani@gmail.com>
2022-08-28 11:49:30 +0200

    filter data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+function fiveAndGreaterOnly (data){
+  return data.filter(x => x > 5)
+}
+
+const evensOnly = (data) => {
+    return data.filter( x => x % 2 === 0)
+}
 
 function App() {
+    const fiveAndGreater = fiveAndGreaterOnly([3, 6, 8, 2]); /// [6, 8]
+    const evens = evensOnly([3, 6, 8, 2]); /// [6, 8, 2]
   return (
     <>
-    app
+      fiveAndGreater: {fiveAndGreater.map(x => <p> {x} </p>)}
+        evensOnly = {evens.map ( x=> <>{x}, </> )}
+      <br/>
     </>
   );
 }
