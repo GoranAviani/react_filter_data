6679496b5a1982b09c14564b6d9c30efe6d42473
goranaviani <goranaviani@gmail.com>
2022-08-28 11:54:17 +0200

    filter data

--- src/App.js
+++ src/App.js
@@ -10,16 +10,30 @@ const fiveCharactersOrFewerOnly = (data) => {
     return data.filter( x => x.length <= 5)
 }
 
+const peopleWhoBelongToTheMovieClub = (data) => {
+    return data.filter(x => x.member === true)
+}
+
 
 function App() {
     const fiveAndGreater = fiveAndGreaterOnly([3, 6, 8, 2]); /// [6, 8]
     const evens = evensOnly([3, 6, 8, 2]); /// [6, 8, 2]
     const chars = fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"]); // ["by", "dog", "wolf", "eaten"]
-  return (
+    const club = peopleWhoBelongToTheMovieClub([
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: false },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+]);
+
+
+    return (
     <>
       fiveAndGreater: {fiveAndGreater.map(x => <p> {x} </p>)}
-        evensOnly = {evens.map ( x=> <>{x}, </> )}
-        fiveCharactersOrFewerOnly = {chars.map ( x=> <li>{x}</li>)}
+        evensOnly: {evens.map ( x=> <>{x}, </> )}
+        fiveCharactersOrFewerOnly: {chars.map ( x=> <li>{x}</li>)}
+        peopleWhoBelongToTheMovieClub: {club.map (x=> <p>Name: {x.name} member: {x.member.toString()}</p>)}
       <br/>
     </>
   );
